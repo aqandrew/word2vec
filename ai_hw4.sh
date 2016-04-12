a1f2e25f0b66a608eeb2d8e3be58f8398e196e98
dawneraq <dawneraq@gmail.com>
2016-04-12 01:19:20 -0400

    Added triggerdata training file on which to train word2vec

--- ai_hw4.sh
+++ ai_hw4.sh
@@ -3,5 +3,5 @@ if [ ! -e text8 ]; then
   wget http://mattmahoney.net/dc/text8.zip -O text8.gz
   gzip -d text8.gz -f
 fi
-time ./word2vec -train ../triggerdata/per_alternate_names.TriggerWordList -output ai_hw4_vectors.bin
+time ./word2vec -train training_file -output ai_hw4_vectors.bin
 ./distance ai_hw4_vectors.bin
